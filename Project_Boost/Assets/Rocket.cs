d617d01533421ed2aeab4b46512a9ec5ac39a7bb
Enze Zheng <zhengenze1994@hotmail.com>
2018-10-01 00:16:03 -0700

    Initial the rocket
    build up the rock and make it able to fly

---
+++ Project_Boost/Assets/Rocket.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    Rigidbody rigidbody;
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();
+	}
+
+    private void ProcessInput()
+    {
+        if(Input.GetKey(KeyCode.A)&&!Input.GetKey(KeyCode.D))
+        {
+            print("Rotating Left");
+            transform.Rotate(Vector3.forward);
+        }
+        if(Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
+        {
+            print("Rotating Right");
+            transform.Rotate(Vector3.back);
+        }
+        if(Input.GetKey(KeyCode.Space))//Can thrust while rotating
+        {
+            //print("Thrusting");
+            rigidbody.AddRelativeForce(Vector3.up);
+        }
+    }
+}
